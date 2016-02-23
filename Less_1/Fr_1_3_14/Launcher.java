8c92ac3782a330b20f223fd04f9091af55eca20d
Sergii <sergii.chukhno@gmail.com>
2016-02-23 21:57:36 +0200

    create launcher class

---
+++ Less_1/Fr_1_3_14/Launcher.java
@@ -0,0 +1,10 @@
+package Less_1.Fr_1_3_14;
+
+public class Launcher {
+
+	public static void main(String[] args) throws Exception {
+		ActionField af = new ActionField();
+		af.runTheGame();				
+	}
+
+}
