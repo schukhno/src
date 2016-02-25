ad344d94e6ac0baaede81c35130e4e504bea4fc7
Sergii <sergii.chukhno@gmail.com>
2016-02-25 19:05:24 +0200

    book store was added

---
+++ Less_2/Fr_1_1_7/BookGenre.java
@@ -0,0 +1,5 @@
+package Less_2.Fr_1_1_7;
+
+public enum BookGenre {
+	Fantasy, Food, Computers, Fiction, History
+}
---
+++ Less_2/Fr_1_1_7/BookStore.java
@@ -0,0 +1,18 @@
+package Less_2.Fr_1_1_7;
+
+public class BookStore {
+	private String[][] bookArray;  
+	private int bookName = 0;
+	private int bookAuthor = 1;
+	private int bookGenre = 2;
+	
+	public void initializeBookArray(){
+		//bookArray = new String[100][3]; 
+		bookArray = new String[][]{
+			{"1","2","3"},
+			{"1","2","3"}
+		};
+		
+		 
+	}
+}
