51be90a0cfd6cdba21f4fe48af4bd74d33f2ea82
Sergii <sergii.chukhno@gmail.com>
2016-02-26 17:02:53 +0200

    Launcher added

--- Less_2/Fr_1_1_7/BookStore.java
+++ Less_2/Fr_1_1_7/BookStore.java
@@ -6,13 +6,21 @@ public class BookStore {
 	private int bookAuthor = 1;
 	private int bookGenre = 2;
 	
-	public void initializeBookArray(){
+	//BookGenre bookGenre1;
+	
+	public BookStore(){
 		//bookArray = new String[100][3]; 
 		bookArray = new String[][]{
-			{"1","2","3"},
+			{"The Deep End of the Ocean","Jacquelyn Mitchard","BookGenre.Fantasy"},
 			{"1","2","3"}
-		};
-		
-		 
+		};				
+	}
+	
+	public void printArray(){
+		for (int i = 0; i< bookArray.length; i++){
+				System.out.println(bookArray[i][bookName]+" "+
+		                           bookArray[i][bookAuthor]+" "+
+						           bookArray[i][bookGenre]);
+		}
 	}
 }
---
+++ Less_2/Fr_1_1_7/Launcher.java
@@ -0,0 +1,11 @@
+package Less_2.Fr_1_1_7;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		BookStore bookStore = new BookStore();
+		
+		bookStore.printArray();
+	}
+
+}
