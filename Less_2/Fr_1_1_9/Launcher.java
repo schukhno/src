b68958048d74a96c13bd3acef901657a36f94d8e
Sergii <sergii.chukhno@gmail.com>
2016-02-27 21:01:20 +0200

    Tank was updated

---
+++ Less_2/Fr_1_1_9/Launcher.java
@@ -0,0 +1,10 @@
+package Less_2.Fr_1_1_9;
+
+public class Launcher {
+
+	public static void main(String[] args) throws Exception {
+		ActionField af = new ActionField();
+		af.runTheGame();				
+	}
+
+}
