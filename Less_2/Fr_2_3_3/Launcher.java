7b242025ae30a744e93ccd29976a5ecbfc018dd7
Sergii <sergii.chukhno@gmail.com>
2016-02-28 20:22:18 +0200

    Direction enum was added

---
+++ Less_2/Fr_2_3_3/Launcher.java
@@ -0,0 +1,13 @@
+package Less_2.Fr_2_3_3;
+
+public class Launcher {
+
+	public static void main(String[] args) throws Exception {
+		
+		
+		
+		System.out.println(Direction.values());
+		
+	}
+
+}
