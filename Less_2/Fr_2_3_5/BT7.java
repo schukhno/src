546acb24cea8e4c53b8b8fc3916b6db89751fbc3
Sergii <sergii.chukhno@gmail.com>
2016-02-29 09:38:44 +0200

    updated

--- Less_2/Fr_2_3_5/BT7.java
+++ Less_2/Fr_2_3_5/BT7.java
@@ -2,12 +2,6 @@ package Less_2.Fr_2_3_5;
 
 public class BT7 extends Tank{
 	
-	private int speed;
-	
-	public int getSpeed(){
-		return speed;
-	}
-	
 	public BT7(int x, int y, Direction direction, ActionField af,	BattleField bf) {
 		super(x, y, direction, af, bf);
 		speed = 10;
